fbf35ecf0bc7f0e3e3eeabb630efa8c24efc5b65
simple <2309780282@qq.com>
2019-02-10 20:28:59 +0800

    魔镜初步polygon

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,8 +8,55 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    markers:[],
-    hasMarkers:false //添加这个变量是为了防止map只再初始化渲染一次的bug
+    //markers:[],
+    count:0,
+    hasMarkers:false, //添加这个变量是为了防止map只再初始化渲染一次的bug
+    sub_count: 0,
+    magnifier: false
+  },
+  //查询点是否在面内
+  isPointInPolygon:function(iterable,latitude,longitude){
+    var iSum, iCount, iIndex;
+    var dLon1 = 0, dLon2 = 0, dLat1 = 0, dLat2 = 0, dLon;
+    var ALat = latitude;
+    var ALon = longitude;
+    if (iterable.length < 3) {
+      return false;
+    }
+    iSum = 0;
+    iCount = iterable.length;
+    
+    //List <BPoint> ps = mapSheet.BPoints;
+    for (iIndex = 0; iIndex < iCount; iIndex++) {
+      if (iIndex == iCount - 1) {
+        dLon1 = iterable[iIndex].longitude;
+        dLat1 = iterable[iIndex].latitude;
+        dLon2 = iterable[0].longitude;
+        dLat2 = iterable[0].latitude;
+      }
+      else {
+        dLon1 = iterable[iIndex].longitude;
+        dLat1 = iterable[iIndex].latitude;
+        dLon2 = iterable[iIndex + 1].longitude;
+        dLat2 = iterable[iIndex + 1].latitude;
+      }
+      // 以下语句判断A点是否在边的两端点的水平平行线之间，在则可能有交点，开始判断交点是否在左射线上  
+      if (((ALat >= dLat1) && (ALat < dLat2)) || ((ALat >= dLat2) && (ALat < dLat1))) {
+        if (Math.abs(dLat1 - dLat2) > 0) {
+          //得到 A点向左射线与边的交点的x坐标： 
+          //dLon = ((ALon - dLon1) * (dLon2 - dLon1) * (dLat1 - dLat2) + (dLat1 - dLat2) * (dLat1 - dLat2) * ALat + (dLon2 - dLon1) * (dLon2 - dLon1) * dLat1) / ((dLat1 - dLat2) * (dLat1 - dLat2) + (dLon2 - dLon1) * (dLon2 - dLon1));
+          dLon = dLon1 - ((dLon1 - dLon2) * (dLat1 - ALat)) / (dLat1 - dLat2);
+          // 如果交点在A点左侧，则射线与边的全部交点数加一：  
+          if (dLon < ALon) {
+            iSum++;
+          }
+        }
+      }
+    }
+    if ((iSum % 2) != 0) {
+      return true;
+    }
+    return false;
   },
   createMarkers: function (iterable) {
     var markers = []
@@ -31,6 +78,66 @@ Page({
     wx.navigateTo({
       url: '../logs/logs'
     })
+  },
+  magnifierChange: function (e) {
+    if (e.detail.value) {
+      //开启放大镜功能
+      this.setData({
+        magnifier: true
+      });
+    } else {
+      this.setData({
+        magnifier: false,
+        sub_polygons: [],
+        sub_count: 0,
+        sub_markers: []
+      });
+    }
+
+  },
+  markertap:function(res){
+    if (!this.data.magnifier) {
+      return;
+    }
+    var sub_markers = [];
+    //console.log(res);
+    var lat = ''; // 获取点击的markers经纬度
+    var lon = ''; // 获取点击的markers经纬度
+    var markerId = res.markerId;// 获取点击的markers id
+    var latdiffer = 0.01; //纬差
+    var londiffer = 0.01; //经差
+    const jsonJs = require("./json.js")
+    var dataArray = jsonJs.data.alcohol_data
+    this.setData({
+      alcohol_data: dataArray
+    })
+    var markers = this.createMarkers(this.data.alcohol_data)
+    for (var item of markers) {
+      if (item.id === markerId) {
+        lat = item.latitude;
+        lon = item.longitude;
+        console.log(item);
+      }
+    }
+    lat = parseFloat(lat);
+    lon = parseFloat(lon);
+    var points = [{ latitude: lat - latdiffer, longitude: lon - londiffer }, { latitude: lat - latdiffer, longitude: lon + londiffer }, { latitude: lat + latdiffer, longitude: lon + londiffer }, { latitude: lat + latdiffer, longitude: lon - londiffer}];
+    this.setData({
+      sub_polygons: [{
+        points: points
+      }]
+    });
+    for (var item of markers) {
+        if(this.isPointInPolygon(points,item.latitude,item.longitude)){
+          sub_markers.push(item);
+        }
+    }
+    this.setData({
+      sub_markers: sub_markers,
+      sub_count: sub_markers.length
+    })
+    console.log(sub_markers);
+    console.log(points);
   },
   onLoad: function() {
     if (app.globalData.userInfo) {
@@ -67,8 +174,9 @@ Page({
     })
     var markers = this.createMarkers(this.data.alcohol_data)
     this.setData({
-       markers:markers,
-       hasMarkers:true
+      //这个地方设置markers属性值，不管前面是否定义markers，markers已经有了
+      markers:markers,
+      hasMarkers:true
       })
   },
 
