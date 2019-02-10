9ce1016f4972a6a84b6bb47243b50754df5ebe9d
lsl <1229840757@qq.com>
2019-02-10 12:37:57 +0800

    增加醉酒数据显示功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,24 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    markers:[],
+    hasMarkers:false //添加这个变量是为了防止map只再初始化渲染一次的bug
+  },
+  createMarkers: function (iterable) {
+    var markers = []
+    for (var i = 0; i < iterable.length; i++) {
+      var marker = {
+        iconPath: '../images/markers.png',
+        id: i,
+        latitude: iterable[i].lat,
+        longitude: iterable[i].lng,
+        width: 5,
+        height: 5
+      }
+      markers.push(marker)
+    };
+    return markers;
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +32,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -42,7 +59,19 @@ Page({
         }
       })
     }
+    //这里读取完用户信息之后完成醉酒json文件的载入
+    const jsonJs = require("./json.js")
+    var dataArray = jsonJs.data.alcohol_data
+    this.setData({
+      alcohol_data: dataArray
+    })
+    var markers = this.createMarkers(this.data.alcohol_data)
+    this.setData({
+       markers:markers,
+       hasMarkers:true
+      })
   },
+
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
